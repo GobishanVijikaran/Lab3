a666ce1327b5e427d4686ef4b53f4dcda102beae
Gobishan Vijikaran <gvijikar@edu.uwaterloo.ca>
2020-06-30 12:31:34 -0400

    added lines of thread

--- main.c
+++ main.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
 #include <cmsis_os2.h>
 
-int n = 0; 
 
 // Each function is essentially a new thread (each thread has a void *arg parameter even if there arent any 
 
-void counter(void *arg){
-	while(1){
-		++n; 
-		osDelay(100); 
-	}
-}
+// Thread to read the joystick 
 
-void printer(void *arg){
-	int last = 0; 
-	while(1){
-		if (n != last){
-			printf("%s %d\n", (char *) arg, n); 
-		}
-		last = n; 
-		osThreadYield();
-	}
-}
+// Thread to read values from the ADC and print them out to serial port 
+
+// Thread to toggle the LED
+
 
 int main(){
 	osKernelInitialize(); 
-	osThreadNew(counter, NULL, NULL); 
-	osThreadNew(printer, "Printer says: ", NULL); 
 	osKernelStart();  
 }
